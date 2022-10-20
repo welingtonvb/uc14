dd9f6d6a96b94f4a0b56fcb422f716e53aa37007
welingtonvb <98372117+welingtonvb@users.noreply.github.com>
2022-10-20 19:40:35 -0300

    Update remoto 1 e 2 readme.md

---
+++ Encontro remoto 2/Chapter/Chapter/Controllers/LivroController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Chapter.Repositories;
+using Chapter.Models;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livrobuscado = _livroRepository.BuscarPorId(id);
+
+                if (livrobuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livrobuscado);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro removido com suscesso");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id,Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Encontro remoto 2/Script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Chapter;
+
+USE Chapter;
+
+CREATE TABLE Livros 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Titulo VARCHAR(150) NOT NULL,
+	QuantidadePaginas INT,
+	Disponivel BIT
+);
+
+INSERT INTO Livros VALUES ('Titulo A', 100, 1);
+INSERT INTO Livros VALUES ('Titulo B', 200, 0);
+INSERT INTO Livros VALUES ('Titulo B', 300, 0);
+
+SELECT * FROM Livros;
