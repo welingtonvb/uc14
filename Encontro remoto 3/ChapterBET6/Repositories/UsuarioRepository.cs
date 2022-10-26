337684621b7065d8e58c6aa9056e35042d151404
welingtonvb <98372117+welingtonvb@users.noreply.github.com>
2022-10-26 16:38:00 -0300

    Encontro remoto 3_upd1

---
+++ Encontro remoto 3/ChapterBET6/Repositories/UsuarioRepository.cs
@@ -0,0 +1,89 @@
+﻿using ChapterBET6.Contexts;
+using ChapterBET6.Interfaces;
+using ChapterBET6.Models;
+
+namespace ChapterBET6.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly SqlContext _context;
+
+        public UsuarioRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="usuario"></param>
+        public void Atualizar(int id, Usuario usuario)
+        {
+            Usuario usuarioEncontrado = _context.Usuarios.Find(id);
+
+            if (usuarioEncontrado != null)
+            {
+                usuarioEncontrado.Email = usuario.Email;
+                usuarioEncontrado.Senha = usuario.Senha;
+                usuarioEncontrado.Tipo = usuario.Tipo;
+
+                _context.Usuarios.Update(usuarioEncontrado);
+
+                _context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Usuario BuscarPorId(int id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usuario"></param>
+        public void Cadastrar(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        public void Deletar(int id)
+        {
+            Usuario usuarioEncontrado = _context.Usuarios.Find(id);
+            _context.Usuarios.Remove(usuarioEncontrado);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Usuario> Listar()
+        {
+            return _context.Usuarios.ToList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="senha"></param>
+        /// <returns></returns>
+        public Usuario Login(string email, string senha)
+        {
+            return _context.Usuarios.FirstOrDefault(x => x.Email == email && x.Senha == senha);
+        }
+    };
+}
+
